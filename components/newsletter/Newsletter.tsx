0c32901d0eb99c7adf5ca69db446cbd7a3dfb25f
gdbroman <99gustaf@gmail.com>
2022-07-17 00:01:59 +0200

    rev

--- components/newsletter/Newsletter.tsx
+++ components/newsletter/Newsletter.tsx
@@ -14,10 +14,7 @@ export const Newsletter = ({ standalone }: NewsletterProps) => (
       <Image src="/images/tse.jpeg" alt={greeting} width={100} height={100} />
     </div>
     <h1>{greeting}</h1>
-    <p>
-      I build things for the web. But I also love ideas and writing (especially in markdown), so
-      every Sunday I document lessons from being a dev.
-    </p>
+    <p>I build things for the web. Every Sunday I document lessons from being a dev.</p>
     <GitLetterForm />
   </div>
 );
