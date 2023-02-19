939260ca3c6b0e54394d1ba5135c56c967980131
gdbroman <99gustaf@gmail.com>
2023-02-19 15:19:31 +0100

    Get rid of book cope

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,50 +1,26 @@
 import Image from 'next/image';
 
 import GithubIcon from './icons/github.svg';
-import GoodreadsIcon from './icons/goodreads.svg';
 import TwitterIcon from './icons/twitter.svg';
 import UrbitIcon from './icons/urbit.svg';
 
-type Path = {
-  href: string;
-  alt: string;
-  icon: typeof GithubIcon;
-};
-
-const socials: Path[] = [
-  {
-    href: 'https://github.com/gdbroman',
-    alt: 'Gustaf Broman on Github',
-    icon: GithubIcon
-  },
-  {
-    href: 'https://twitter.com/gdbroman',
-    alt: 'Gustaf Broman on Twitter',
-    icon: TwitterIcon
-  },
-  {
-    href: 'https://www.goodreads.com/gdbroman',
-    alt: 'Gustaf Broman on Goodreads',
-    icon: GoodreadsIcon
-  }
-];
-
 const urbitId = '~lopsyp-doztun';
 
-const copyUrbitIdToClipBoard = () => {
-  navigator.clipboard.writeText(urbitId);
-};
+const copyUrbitIdToClipBoard = () => navigator.clipboard.writeText(urbitId);
 
-export const Footer = (): JSX.Element => (
+export const Footer = () => (
   <footer>
     <ul>
-      {socials.map(({ href, icon, alt }) => (
-        <li key={href}>
-          <a href={href} target="_blank" rel="noreferrer" title={`Go to ${alt}`}>
-            <Image src={icon} alt={alt} />
-          </a>
-        </li>
-      ))}
+      <li>
+        <a href="https://github.com/gdbroman" target="_blank" rel="noreferrer">
+          <Image src={GithubIcon} alt="https://github.com/gdbroman" />
+        </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/gdbroman" target="_blank" rel="noreferrer">
+          <Image src={TwitterIcon} alt="https://twitter.com/gdbroman" />
+        </a>
+      </li>
       <li>
         <a
           role="button"
@@ -52,7 +28,7 @@ export const Footer = (): JSX.Element => (
           title="Copy Urbit ID to clipboard"
           onClick={copyUrbitIdToClipBoard}
         >
-          <Image src={UrbitIcon} alt="Gustaf Broman on Urbit" />
+          <Image src={UrbitIcon} alt={urbitId} />
         </a>
       </li>
     </ul>
