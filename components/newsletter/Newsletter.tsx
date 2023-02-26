e1a9918e871646fae75cdbec24fdb5c990b137ac
gdbroman <99gustaf@gmail.com>
2023-02-26 11:55:49 +0100

    web

--- components/newsletter/Newsletter.tsx
+++ components/newsletter/Newsletter.tsx
@@ -13,7 +13,7 @@ export const Newsletter = ({ standalone }: NewsletterProps) => (
       <Image src="/images/profile.jpeg" alt={greeting} width={100} height={100} />
     </div>
     <h1>{greeting}</h1>
-    <p>I&apos;m a self-taught front-end engineer.</p>
+    <p>I&apos;m a self-taught web engineer.</p>
     <p>
       Here I share my best lessons on how to navigate the information age as a dev. Get it straight
       to your inbox:
